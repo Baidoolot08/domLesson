473d73c4aa0cb11365e9dc49368705e35fb3273f
Baidoolot08 <bajdoolotbaatyrov@gmail.com>
2024-12-28 11:11:45 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,60 @@
+let title = document.querySelector(".card");
+title.style.background = "black";
+title.style.width = "646px";
+title.style.height = "700px";
+title.style.color = " white";
+title.style.borderRadius = "5%";
+title.style.fontSize = " 15px";
+title.style.marginLeft = "460px";
+title.style.marginTop = " 60px";
+title.style.gap = "45px";
+
+let img = document.querySelector("img");
+img.style.width = " 300px";
+img.style.height = "300px";
+img.style.borderRadius = " 10%";
+img.style.marginLeft = " 180px";
+img.style.marginTop = "50px";
+let hol = document.querySelector("h1");
+hol.style.marginLeft = "268px";
+hol.style.color = " white";
+let holl = document.querySelector("h3");
+holl.style.marginLeft = " 287px";
+holl.style.color = " white";
+let tal = document.querySelector(".about");
+tal.style.display = "flex";
+tal.style.gap = "80px";
+tal.style.marginLeft = "-40px";
+
+let tall = document.querySelector(".about h1");
+tall.style.marginLeft = "140px";
+tall.style.textAlign = "center";
+let y = document.querySelector(".m");
+y.style.marginLeft = "50px";
+let k = document.querySelector(".n");
+k.style.marginLeft = "40px";
+
+let ham = document.querySelector(".card button");
+
+ham.style.border = "none";
+
+ham.style.marginLeft = "100px";
+ham.style.marginTop = "20px";
+
+let am = document.querySelector(".card button a");
+am.style.color = " white";
+am.style.textDecoration = "none";
+let a = document.querySelector(".YouTube ");
+a.style.width = " 130px";
+a.style.height = "30px";
+a.style.marginLeft = "100px";
+a.style.color = "white";
+
+let b = document.querySelector(".glow-on-hover ");
+b.style.width = " 130px";
+b.style.height = "30px";
+b.style.marginLeft = "30px";
+let c = document.querySelector(".hover ");
+c.style.width = " 130px";
+c.style.height = "30px";
+c.style.marginLeft = "20px";
